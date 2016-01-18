d0610cce5d9bef941cd5ea6770de45f4b0d427d3
Lebda <lebsoft@seznam.cz>
2016-01-18 14:17:34 +0100

    Model2

---
+++ LigaPetiWeb/LigaPetiDbModel/DataContext/Identity/IdentityDb.cs
@@ -0,0 +1,19 @@
+﻿using LigaPetiDbModel.Entities.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace LigaPetiDbModel.DataContext.Identity
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class IdentityDb : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDb()
+            : base("LigaPetiWeb", throwIfV1Schema: false)
+        {
+        }
+
+        public static IdentityDb Create()
+        {
+            return new IdentityDb();
+        }
+    }
+}
